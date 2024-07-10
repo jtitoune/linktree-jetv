1d48576347d6973d875da818c79654a96f8280cd
José Tito Valenzuela <71992579+jtitoune@users.noreply.github.com>
2024-07-10 15:14:27 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.type', {
+    strings: [
+    'Hola',' Me llamo José Tito Valenzuela.', 'Me encanta la Informática.', 'Me encanta enseñar.'],
+    typeSpeed:100,
+    backSpeed:70,
+    loop:true,
+    });
